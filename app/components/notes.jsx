521a4f1afe33ca601b8c099befb4d2c1dddd85b3
Jon Seidel <jseidel@edpci.com>
2017-05-13 05:40:29 -0700

    Finish 3.3

--- app/components/notes.jsx
+++ app/components/notes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import uuid from 'uuid';
 
 const notes = [
-  { id: '12345-abcde', task: 'Learn React'},
-  { id: 'abcde-12345', task: 'Use Webpack'},
-  { id: '12345-67890', task: 'Up Your Game'}
+  { id: uuid.v4(), task: 'Learn React'},
+  { id: uuid.v4(), task: 'Use Webpack'},
+  { id: uuid.v4(), task: 'Up Your Game'}
 ];
-
 export default () => (
   <ul>{notes.map(note =>
     <li key={note.id}>{note.task}</li>
