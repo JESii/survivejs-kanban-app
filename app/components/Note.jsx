fbc6954ba0ef10505d6026d2d9ee235dd8d31471
Jon Seidel <jseidel@edpci.com>
2017-05-19 17:46:54 -0700

    I.6.4 - Editable

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-export default ({task, onDelete}) => (
-  <div>
-    <span>{task}</span>
-    <button onClick={onDelete}> - Delete </button>
+export default({children, ...props}) => (
+  <div {...props}>
+    {children}
   </div>
 );
+// export default ({task, onDelete}) => (
+//   <div>
+//     <span>{task}</span>
+//     <button onClick={onDelete}> - Delete </button>
+//   </div>
+// );
 // Could also be expressed as:
 // export default (props) => <div{props.task}</div>;
 
--- app/components/notes.jsx
+++ app/components/notes.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import Note from './Note';
+import Editable from './Editable';
 
-export default ({notes, onDelete=() => {}}) => (
-  <ul>{notes.map(({id, task}) =>
-    <li key={id}><Note 
-        onDelete={onDelete.bind(null, id)}
-        task={task} />
+export default ({
+  notes,
+  onNoteClick = () => {},
+  onEdit = () => {},
+  onDelete = () => {}
+}) => (
+  <ul>{notes.map(({id, editing, task}) =>
+    <li key={id}>
+      <Note onClick={onNoteClick.bind(null, id)}>
+        <Editable
+          editing={editing}
+          value={task}
+          onEdit={onEdit.bind(null, id)} />
+        <button onClick={onDelete.bind(null, id)}>- Delete</button>
+      </Note>
     </li>
-  )}
-</ul>
-);
-// Note the simplification of the map function
-// Shortened from <ul>{notes.map( (note) => (
+  )}</ul>
+)
+  // <ul>{notes.map(({id, task}) =>
+  //   <li key={id}>
+  //     <Note>
+  //       <span>{task}</span>
+  //       <button>onClick={onDelete.bind(null, id)}> x Delete</button>
+  //     </Note>
+  //   </li>
+  // )}
+  // </ul>
+  // Note the simplification of the map function
+  // Shortened from <ul>{notes.map( (note) => (
